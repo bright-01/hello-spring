0c0c9a25159d45f1fd379b6d6ba58f71628c997e
mwchoen <cheonmyeongwon@gmail.com>
2022-03-12 17:35:27 +0900

    스프링 빈과 의존관계 - 자바 코드로 직접 스프링 빈 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -8,8 +8,19 @@ import org.springframework.stereotype.Controller;
 public class MemberController {
     private final MemberService memberService;
 
+    // DI 3가지 방법
+    // 1. 생성자 주입 가장 권장한다
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    // 2. 필드 주입 권장 하지 않음.. 중간에 변경이 불가능
+//    @Autowired private MemberService memberService1;
+
+    // 3. setter 주입 권장하지 않음. public으로 메서드가 열여있어야함.
+//    public void setMemberService(MemberService memberService){
+//        this.memberService = memberService;
+//    }
+//
 }
