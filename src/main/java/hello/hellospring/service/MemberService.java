6b4b0a723ae26e56a017b34969b271bc37da539d
mwchoen <cheonmyeongwon@gmail.com>
2022-03-12 22:50:50 +0900

    스프링 DB접근 기술 - jpa

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,11 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-
+@Transactional // JPA를 사용하려면 항상 transactional이 있어야 한다.
 public class  MemberService {
     private final MemberRepository memberRepository;
 
